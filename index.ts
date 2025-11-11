e2831ed67a88cd50cacb1d2c286e8b02337c1058
owl352 <kokosinca123@gmail.com>
2025-11-11 03:55:43 +0300

    update exports

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
-import {DashPlatformProtocol} from "./src/types";
-import {DPP} from "./src/dpp";
+import {setDpp} from "./src/dpp";
 import * as protocol from 'pshenmic-dpp-napi';
 
-const dpp: DPP = new DPP(protocol);
-
-export const IdentifierWASM = DPP.IdentifierWASM;
-export const IdentityWASM = DPP.IdentityWASM;
-export const IdentityPublicKeyWASM = DPP.IdentityPublicKeyWASM;
+setDpp(protocol);
 
 export {KeyType, NetworkWASM, PlatformVersionWASM, Purpose, SecurityLevel} from "./src/types";
+export {IdentityWASM, IdentifierWASM, IdentityPublicKeyWASM} from "./src/dpp"
--- src/dpp.ts
+++ src/dpp.ts
@@ -3,23 +3,12 @@ import * as Identifier from "./structs/Identifier";
 import * as Identity from "./structs/Identity";
 import * as IdentityPublicKey from "./structs/IdentityPublicKey";
 
-export type { IdentityWASM } from './structs/Identity';
-export type { IdentifierWASM } from './structs/Identifier';
-export type { IdentityPublicKeyWASM } from './structs/IdentityPublicKey';
+export {IdentifierWASM} from "./structs/Identifier"
+export {IdentityWASM} from "./structs/Identity"
+export {IdentityPublicKeyWASM} from "./structs/IdentityPublicKey"
 
-export class DPP {
-  rawDpp: DashPlatformProtocol;
-
-  //structs
-  static IdentifierWASM = Identifier.IdentifierWASM;
-  static IdentityWASM = Identity.IdentityWASM;
-  static IdentityPublicKeyWASM = IdentityPublicKey.IdentityPublicKeyWASM;
-
-  constructor(dpp: DashPlatformProtocol) {
-    this.rawDpp = dpp
-
-    Identifier.setDpp(dpp);
-    Identity.setDpp(dpp);
-    IdentityPublicKey.setDpp(dpp);
-  }
+export function setDpp(dpp: DashPlatformProtocol) {
+  Identifier.setDpp(dpp);
+  Identity.setDpp(dpp);
+  IdentityPublicKey.setDpp(dpp);
 }
